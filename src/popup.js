17ee52a9e222fa705a6846314fc49c09850530ec
Muhammad Ammad <me.ammad1786@gmail.com>
2025-03-23 07:39:02 +0500

    It now recieves message from the website and stores it inside the localStorage

--- src/popup.js
+++ src/popup.js
@@ -1,23 +1,6 @@
-document.getElementById("signin").addEventListener("click", () => {
-  //   chrome.runtime.sendMessage({ action: "signin" });
-  chrome.runtime.sendMessage({ message: "login" }, function (response) {
-    if (response === "success") window.close();
+document.getElementById("signin").addEventListener("click", function () {
+  chrome.storage.local.get(null, (data) => {
+    alert(`Stored JWT Token: ${data.jwt || "No token found!"}`);
+    console.log("Stored JWT Token:", data);
   });
 });
-
-// // Listen for storage updates
-// chrome.storage.local.get(["user", "emails"], (data) => {
-//   if (data.user) {
-//     document.getElementById("user-info").innerHTML = `
-//         <h2>Welcome, ${data.user.email}</h2>
-//       `;
-//   }
-//   if (data.emails) {
-//     document.getElementById("emails").innerHTML = `
-//         <h3>Your Emails:</h3>
-//         <ul>
-//           ${data.emails.map((email) => `<li>${email.id}</li>`).join("")}
-//         </ul>
-//       `;
-//   }
-// });
