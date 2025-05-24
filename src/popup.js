fc4f4c1efed145d96ab026290d404b24f3b20a27
Muhammad Ammad <me.ammad1786@gmail.com>
2025-05-25 03:32:43 +0500

    Designed UI for authorized and unauthorized popups

--- src/popup.js
+++ src/popup.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "UPDATE_POPUP" && message.name) {
       document.querySelector(".username").innerText = message.name;
-      document.querySelector(".authorized").style.display = "block";
+      document.querySelector(".authorized").style.display = "flex";
       document.querySelector(".unauthorized").style.display = "none";
     }
   });
@@ -12,20 +12,22 @@ document.addEventListener("DOMContentLoaded", function () {
   chrome.storage.local.get(null, (data) => {
     if (data.name) {
       document.querySelector(".username").innerText = data.name;
-      document.querySelector(".authorized").style.display = "block";
+      document.querySelector(".authorized").style.display = "flex";
       document.querySelector(".unauthorized").style.display = "none";
     } else {
-      document.querySelector(".authorized").style.display = "none";
+      document.querySelector(".authorized").style.display = "flex";
       document.querySelector(".unauthorized").style.display = "block";
     }
   });
 });
-document.getElementById("redirectBtn").addEventListener("click", (e) => {
-  e.preventDefault();
-  const url = config.dashboard_url;
-  if (url) {
-    window.open(url, "_blank");
-  } else {
-    console.error("Dashboard URL is not defined in the config.");
-  }
+document.querySelectorAll(".redirectBtn").forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const url = config.dashboard_url;
+    if (url) {
+      window.open(url, "_blank");
+    } else {
+      console.error("Dashboard URL is not defined in the config.");
+    }
+  });
 });
